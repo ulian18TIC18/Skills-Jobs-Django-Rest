495d2ebac3fd95002b8c1e226d0f570895dcab25
ulian18TIC18 <ulian18@gmail.com>
2024-10-15 15:03:24 -0300

    Criando metodos GET e POST como endpoints http serializers

--- Sistema-Vaga-Emprego-Rest/core/settings.py
+++ Sistema-Vaga-Emprego-Rest/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'skills',
     'jobs',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- Sistema-Vaga-Emprego-Rest/skills/views.py
+++ Sistema-Vaga-Emprego-Rest/skills/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework import status
+
+from .models import Skill
+from .serializers import SkillSerializer
+
+
+class SkillListView(APIView):
+    def get(self, request): 
+        skills = Skill.objects.all()
+        serializer = SkillSerializer(
+            skills,
+            many = True
+        )
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = SkillSerializer(
+            data=request.data
+            )
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)        
+       
