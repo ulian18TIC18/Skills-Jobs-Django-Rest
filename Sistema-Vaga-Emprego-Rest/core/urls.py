19f7295e97386453ab94c262033dcea136d41116
ulian18TIC18 <ulian18@gmail.com>
2024-10-16 02:48:51 -0300

    Incluindo o crud de jobs e skills

--- Sistema-Vaga-Emprego-Rest/core/urls.py
+++ Sistema-Vaga-Emprego-Rest/core/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/skills/", include('skills.urls', namespace='skills')),   
+    path("api/skills/", include('skills.urls', namespace='skills')),
+    path('api/jobs/', include('jobs.urls', namespace='jobs')),   
 ]
--- Sistema-Vaga-Emprego-Rest/jobs/views.py
+++ Sistema-Vaga-Emprego-Rest/jobs/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Job
+from .serializers import JobSerializer
 
 # Create your views here.
+
+class JobViewSet(ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
